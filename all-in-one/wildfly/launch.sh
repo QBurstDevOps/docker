66dbc655189bf16e42799c3f7ef63725b2ca3031
Mithun Cheriyath <mithun@qburst.com>
2014-10-23 05:10:50 -0400

    Files for wildfly docker

---
+++ all-in-one/wildfly/launch.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+#IPADDR=$(ip a s | sed -ne '/127.0.0.1/!{s/^[ \t]*inet[ \t]*\([0-9.]\+\)\/.*$/\1/p}')
+IPADDR=0.0.0.0
+/wildfly/bin/standalone.sh -c standalone-ha.xml -Djboss.bind.address=$IPADDR -Djboss.bind.address.management=$IPADDR -Djboss.node.name=server-$IPADDR
