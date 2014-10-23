985991aab5ff3d1edb37bf00239754628c7a54d3
QBurstDevOps <devops@qburst.com>
2014-10-23 12:24:02 +0530

    Update launch.sh

--- all-in-one/wildflyapache/launch.sh
+++ all-in-one/wildflyapache/launch.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-IPADDR=$(ip a s | sed -ne '/127.0.0.1/!{s/^[ \t]*inet[ \t]*\([0-9.]\+\)\/.*$/\1/p}')
-/opt/wildfly/bin/standalone.sh -c standalone-ha.xml -Djboss.bind.address=$IPADDR -Djboss.bind.address.management=$IPADDR -Djboss.node.name=server-$IPADDR
+#IPADDR=$(ip a s | sed -ne '/127.0.0.1/!{s/^[ \t]*inet[ \t]*\([0-9.]\+\)\/.*$/\1/p}')
+
+/opt/wildfly/bin/standalone.sh -c standalone-ha.xml -Djboss.bind.address=0.0.0.0 -Djboss.bind.address.management=0.0.0.0 -Djboss.node.name=server-0.0.0.0
 /bin/bash -c "source /etc/apache2/envvars && exec /usr/sbin/apache2 -DFOREGROUND"
